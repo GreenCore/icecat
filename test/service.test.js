28510b228d977dfac28353f97d700ab5d07b2467
Alexander Dransfield <alex@dransfield.me>
2019-07-05 11:02:12 +0100

    Converting tests to ava, full test coverage

--- test/service.test.js
+++ test/service.test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const PassThrough = require('stream').PassThrough;
 const sinon = require('sinon');
-const test = require('tape');
+const test = require('ava');
 
 const IcecatService = require('../lib/OpenCatalog/service');
 const IcecatProduct = require('../lib/OpenCatalog/product');
@@ -18,15 +18,22 @@ const instance = {
   lang: 'EN'
 };
 
+let sandbox;
+test.beforeEach(() => {
+  sandbox = sinon.createSandbox();
+});
+
+test.afterEach(() => {
+  sandbox.restore();
+});
+
 test('_getBaseUrl returns correct base URL', (t) => {
   const service = new IcecatService(instance);
   const expectedUrl = `${instance.scheme}${instance.httpAuth}@${instance.httpUrl}?lang=${instance.lang};output=productxml`;
   t.is(service._getBaseUrl(instance.lang), expectedUrl);
-  t.end();
 });
 
-test('getProduct calls correct url', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial('getProduct calls correct url', (t) => {
   const service = new IcecatService(instance);
   sandbox.stub(service, '_requestProduct');
 
@@ -36,13 +43,10 @@ test('getProduct calls correct url', (t) => {
 
   const expectedUrl = service._getBaseUrl(lang) + `;ean_upc=${barcode}`;
 
-  t.ok(service._requestProduct.calledWith(expectedUrl));
-  sandbox.restore();
-  t.end();
+  t.truthy(service._requestProduct.calledWith(expectedUrl));
 });
 
-test('getProductById calls correct url', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial('getProductById calls correct url', (t) => {
   const service = new IcecatService(instance);
   sandbox.stub(service, '_requestProduct');
 
@@ -54,12 +58,9 @@ test('getProductById calls correct url', (t) => {
   const callArg = service._requestProduct.getCall(0).args[0];
 
   t.is(callArg, expectedUrl);
-  sandbox.restore();
-  t.end();
 });
 
-test('getProductBySKU calls correct url', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial('getProductBySKU calls correct url', (t) => {
   const service = new IcecatService(instance);
   sandbox.stub(service, '_requestProduct');
 
@@ -72,16 +73,14 @@ test('getProductBySKU calls correct url', (t) => {
   const callArg = service._requestProduct.getCall(0).args[0];
 
   t.is(callArg, expectedUrl);
-  sandbox.restore();
-  t.end();
 });
 
-test('getProductByXMLdata can return IcecatProduct', (t) => {
+test.serial.cb('getProductByXMLdata can return IcecatProduct', (t) => {
   const service = new IcecatService();
   const promise = service.getProductByXMLdata(icecatProductXML);
 
   promise.then((icecatProduct) => {
-    t.ok(icecatProduct instanceof IcecatProduct);
+    t.truthy(icecatProduct instanceof IcecatProduct);
     t.end();
   });
 });
@@ -89,27 +88,22 @@ test('getProductByXMLdata can return IcecatProduct', (t) => {
 test('Sets icecat to empty object if initialised with nothing', (t) => {
   const service = new IcecatService();
   t.deepEqual(service.icecat, {});
-  t.end();
 });
 
 function isPromise(x) {
   return x && x instanceof Promise;
 }
 
-test('_requestProduct returns Promise', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial('_requestProduct returns Promise', (t) => {
   const service = new IcecatService(instance);
   sandbox.stub(service.https, 'get').returns({
     on: () => {}
   });
 
-  t.ok(isPromise(service._requestProduct()));
-  sandbox.restore();
-  t.end();
+  t.truthy(isPromise(service._requestProduct()));
 });
 
-test('_requestProduct can return IcecatProduct', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial.cb('_requestProduct can return IcecatProduct', (t) => {
   const service = new IcecatService(instance);
   const requestStream = new PassThrough();
   sandbox.stub(service.https, 'get').returns(requestStream);
@@ -119,32 +113,28 @@ test('_requestProduct can return IcecatProduct', (t) => {
   const promise = service._requestProduct('');
   service.https.get.yield(responseStream);
   promise.then((icecatProduct) => {
-    t.ok(icecatProduct instanceof IcecatProduct);
-    sandbox.restore();
+    t.truthy(icecatProduct instanceof IcecatProduct);
     t.end();
   });
 });
 
-test('_requestProduct can return error if unable to parse XML', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial.cb('_requestProduct can return error if unable to parse XML', (t) => {
   const service = new IcecatService(instance);
   const requestStream = new PassThrough();
   sandbox.stub(service.https, 'get').returns(requestStream);
-  sandbox.spy(service.parseString);
+  sandbox.spy(service, 'parseString');
   const responseStream = new PassThrough();
   responseStream.write('<wtfis></thisxml>');
   responseStream.end();
   const promise = service._requestProduct('');
   service.https.get.yield(responseStream);
   promise.catch(() => {
-    t.is(service.parseString.calledOnce);
-    sandbox.restore();
+    t.truthy(service.parseString.calledOnce);
     t.end();
   });
 });
 
-test('_requestProduct resolves error if http call fails', (t) => {
-  const sandbox = sinon.createSandbox();
+test.serial.cb('_requestProduct resolves error if http call fails', (t) => {
   const service = new IcecatService(instance);
   const requestStream = new PassThrough();
   sandbox.stub(service.https, 'get').returns(requestStream);
@@ -152,9 +142,8 @@ test('_requestProduct resolves error if http call fails', (t) => {
   const promise = service._requestProduct('');
   const errObj = new Error('OOPS');
   requestStream.emit('error', errObj);
-  promise.catch((err) => {
-    t.is(err, errObj);
-    sandbox.restore();
+  promise.catch((error) => {
+    t.is(error, errObj);
     t.end();
   });
 });
